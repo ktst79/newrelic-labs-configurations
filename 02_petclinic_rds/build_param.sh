7a7b5995efffe8c6cb5db372963d30f03f5a1933
Kota Saito <ksaito@newrelic.com>
2020-02-09 06:25:32 +0900

    Add new configuration, which is petclinic w/ RDS (MySQL)

---
+++ 02_petclinic_rds/build_param.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+NR_LICENSEKEY=
+
+NR_APP_NAME=
+
+AWS_KEY_NAME=
+AWS_S3_PATH=s3://
+AWS_AMIROLE_S3ACCESS=
+
+AWS_CF_STACK=nrlabs-02-petclinic-rds
+AWS_CF_TEMPLATE=./resources/cloudformation/cloudformation.yaml 
+
+APP_NAME=spring-petclinic-master
+APP_URL=https://github.com/spring-projects/spring-petclinic/archive/master.zip
+
+FORCE_TO_DELETE_STACK=enable
