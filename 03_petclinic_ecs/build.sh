196fceecaf8fe44d3eb0bc9b62643af394b38469
Kota Saito <ksaito@newrelic.com>
2020-03-07 07:09:28 +0900

    Add pom dependency dynamically

--- 03_petclinic_ecs/build.sh
+++ 03_petclinic_ecs/build.sh
@@ -90,7 +90,13 @@ if [ "${OVERWRITEAPP}" = "true" ] ; then
     echo "Embeddig Browser Agent Footer"
     AGT_FOOTER='  <div th:utext="${T(com.newrelic.api.agent.NewRelic).getBrowserTimingFooter()}"></div>'
     ${SED} -i "/<\/body>/i ${AGT_FOOTER}" ${APP_DIR}/src/main/resources/templates/fragments/layout.html
+
+    echo "Adding dependency to pom.xml for logback w/ New Relic"
+    POM_FRAGMENT=./resources/newrelic_log/pom_fragment.xml
+    ${SED} -n -i -e "/^\s\s<\/dependencies>/r ${POM_FRAGMENT}" -e 1x -e '2,${x;p}' -e '${x;p}' ${APP_DIR}/pom.xml
 fi
+
+exit 0
 
 cp -f resources/newrelic_log/logback.xml app/src/main/resources
 cp -f resources/newrelic_log/logback-test.xml app/src/test/resources
@@ -115,14 +121,10 @@ export MYSQL_DATABASE=${MYSQL_DATABASE}
 export MYSQL_USER=${MYSQL_USER}
 export MYSQL_PASSWORD=${MYSQL_PASSWORD}
 
-if [ "${ENV}" = "ecs_ec2" ] || [ "${ENV}" = "ecs_fargate" ] ; then
+if [ "${ENV}" = "ecs_ec2" ] ; then
     echo "Deploying on AWS ECS..."
     CLUSTER_NAME=${NR_APP_NAME}-cluster
-    if [ "${ENV}" = "ecs_ec2" ] ; then
-        CLUSTER_LAUNCH_TYPE='EC2'
-    else
-        CLUSTER_LAUNCH_TYPE='FARGATE'
-    fi
+    CLUSTER_LAUNCH_TYPE='EC2'
 
     # Get AWS Region and Account ID
     AWS_REGION=`aws configure get region`
@@ -163,6 +165,72 @@ if [ "${ENV}" = "ecs_ec2" ] || [ "${ENV}" = "ecs_fargate" ] ; then
         sleep 10
         echo "rechecking cluster status..."
     done
+
+    echo "Logging in AWS..."
+    $(aws ecr get-login --no-include-email --region ${AWS_REGION})
+
+    echo "Build docker images..."
+    export REPOSITORY_URI_PATH=
+    docker-compose -f docker-compose-ecs.yml build
+
+    for container in `yq -r '.services[] | select(has("build")).container_name' docker-compose-ecs.yml`
+    do
+        REPOSITORY_NAME=${NR_APP_NAME}-${container}
+        REPOSITORY_URI=`aws ecr describe-repositories --repository-name ${REPOSITORY_NAME} | jq -r '.repositories[0].repositoryUri'`
+        if [ "${REPOSITORY_URI}" = "" ] ; then
+            echo "Creating repository ${REPOSITORY_NAME}..."
+            REPOSITORY_URI=`aws ecr create-repository --repository-name ${REPOSITORY_NAME} | jq -r '.repository.repositoryUri'`
+        fi
+
+        echo "Add tag for the image...: ${REPOSITORY_URI}"
+        docker tag ${REPOSITORY_NAME}:latest ${REPOSITORY_URI}:latest
+
+        echo "Registering container image...: ${REPOSITORY_URI}"
+        docker push ${REPOSITORY_URI}:latest
+    done
+
+    REPOSITORY_URI_PATH=`echo "${REPOSITORY_URI}" | cut -d "/" -f1`
+    REPOSITORY_URI_PATH="${REPOSITORY_URI_PATH}/"
+    export REPOSITORY_URI_PATH=${REPOSITORY_URI_PATH}
+
+    echo "Launching ECS..."
+    ecs-cli compose -f docker-compose-ecs.yml down
+    ecs-cli compose -f docker-compose-ecs.yml up
+elif [ "${ENV}" = "ecs_fargate" ] ; then
+    echo "Deploying on AWS ECS..."
+    CLUSTER_NAME=${NR_APP_NAME}-cluster
+    CLUSTER_LAUNCH_TYPE='FARGATE'
+
+    # Get AWS Region and Account ID
+    AWS_REGION=`aws configure get region`
+    AWS_ACCOUNT_ID=`aws sts get-caller-identity --query 'Account' --output text`
+
+    export AWS_REGION=${AWS_REGION}
+    export AWS_ACCOUNT_ID=${AWS_ACCOUNT_ID}
+
+    ecs-cli ps -c ${CLUSTER_NAME}
+    if [ $? = 1 ] ; then
+        # Create cluster if not exists
+        echo "Creating ECS Cluster...: ${CLUSTER_NAME}"
+        ecs-cli configure -c ${CLUSTER_NAME} -r ${AWS_REGION}
+    else
+        echo "ECS Cluster already exists: ${CLUSTER_NAME}"
+    fi
+
+    if [ "${FORCE_TO_DELETE_STACK}" = "enable" ] ; then
+        echo "Stopping existing cluster instance. ${CLUSTER_NAME}"
+        ecs-cli down --cluster ${CLUSTER_NAME} -f
+    fi
+
+    echo "Launching ECS cluster instance ...: ${CLUSTER_NAME}"
+    LAUNCH_INSTANCE_CMD="ecs-cli up --launch-type ${CLUSTER_LAUNCH_TYPE} --cluster ${CLUSTER_NAME}"
+
+    ${LAUNCH_INSTANCE_CMD}
+
+    if [ $? = 1 ] ; then
+        # If cluster instance already exists
+        echo "Cluster instance already running: ${CLUSTER_NAME}"
+    fi
 
     echo "Logging in AWS..."
     $(aws ecr get-login --no-include-email --region ${AWS_REGION})
